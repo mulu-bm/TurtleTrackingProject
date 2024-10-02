9e543bab129584eb389b62272c52cfb875f29fac
mulu-bm <brian.mulu@duke.edu>
2024-10-02 12:34:56 -0400

    Filtered records added to dictionary pair

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -21,6 +21,10 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
+#Create two dictionary pairs
+date_dict = {}
+loaction_dict = {}
+
 #Pretend we read one line of data from the file
 for lineString in line_list:
     # Check if line is a data line
@@ -36,6 +40,11 @@ for lineString in line_list:
     obs_lc = lineData[4]
     obs_lat = lineData[6]
     obs_lon = lineData[7]
+
+    #Determine if location class criteria is met
+    if obs_lc in ("1", "2", "3"):
+        date_dict[record_id] = obs_date
+        loaction_dict[record_id] = (obs_lat, obs_lon)
 
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
